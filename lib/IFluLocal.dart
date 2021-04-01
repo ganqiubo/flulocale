2cfb8253b4087aacdb1fc041548cd63e101f83db
lz_ganqiubo <2443974192@qq.com>
2021-04-01 11:07:25 +0800

    IFluLocal simplify

--- lib/IFluLocal.dart
+++ lib/IFluLocal.dart
@@ -1,3 +1,20 @@
+import 'package:flulocale/flulocale.dart';
+import 'package:flutter/cupertino.dart';
+
 abstract class IFluLocal{
-  String locale(String key);
+  static const String TAG = "Ilocale";
+
+  String localeLan(BuildContext context, String key){
+    if(context==null){
+      print(TAG + ", " + "context is null");
+      return "";
+    }
+    if(key==null){
+      print(TAG + ", " + "key is null");
+      return "";
+    }
+    return FluLocalizations.of(context).value(key);
+  }
+
+  String locale(String key){}
 }
